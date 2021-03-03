1b23138317f40f5cc4abf3d6d0ffda6e5081ec86
RaiBnod <binodrayee@gmail.com>
2021-03-03 13:56:30 +0545

    Issue fix: MQTT message listener

--- mrb/mqtt.py
+++ mrb/mqtt.py
@@ -3,6 +3,7 @@ import time
 
 from rubix_mqtt.mqtt import MqttClientBase
 
+from mrb.mapper import mqtt_to_rest_mapper
 from mrb.utils.singleton import Singleton
 
 logger = logging.getLogger(__name__)
@@ -12,6 +13,9 @@ class MqttClient(MqttClientBase, metaclass=Singleton):
 
     def __init__(self):
         super().__init__()
+
+    def _on_message(self, client, userdata, message):
+        mqtt_to_rest_mapper(client, userdata, message)
 
     def publish_value(self, topic: str, payload: str):
         if not self.config:
